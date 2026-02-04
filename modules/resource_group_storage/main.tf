9a99bf42af88373c73aaa0eaee908f844009a168
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2026-02-04 22:17:20 +0200

    Add Azure resource group and storage account resources

---
+++ modules/resource_group_storage/main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
